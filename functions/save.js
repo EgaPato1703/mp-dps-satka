0e8c6617ec497f87b96435aab2465159c43f61fb
EgaPato1703 <ega.pato@mail.ru>
2025-05-27 01:02:48 +0500

    Update save.js

--- functions/save.js
+++ functions/save.js
@@ -1,16 +1,19 @@
 const axios = require('axios');
 
-// Эти данные будем брать из переменных окружения
 const BIN_ID = process.env.BIN_ID;
 const MASTER_KEY = process.env.MASTER_KEY;
 
 exports.handler = async function(event) {
-    const { body } = event;
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Метод не поддерживается' };
+    }
+
+    const reports = JSON.parse(event.body);
 
     try {
         await axios.put(
             `https://api.jsonbin.io/v3/b/ ${BIN_ID}`,
-            body,
+            reports,
             {
                 headers: {
                     'X-Master-Key': MASTER_KEY,
@@ -24,7 +27,7 @@ exports.handler = async function(event) {
             body: JSON.stringify({ success: true })
         };
     } catch (error) {
-        console.error('Ошибка при сохранении:', error.message);
+        console.log('Ошибка:', error.message);
         return {
             statusCode: 500,
             body: JSON.stringify({ success: false, error: 'Не удалось сохранить данные' })
